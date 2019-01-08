4763c03405294654eabffd5342f79f17959f62dc
subme96379 <42718117+subme96379@users.noreply.github.com>
2019-01-08 16:14:48 -0500

    Update MN10.sh

--- MN10.sh
+++ MN10.sh
@@ -1,35 +1,40 @@
-#!/bin/bash
+1 #!/bin/bash 
+2 
+ 
+3 #Setup Variables 
+4 GREEN='\033[0;32m' 
+5 YELLOW='\033[0;93m' 
+6 RED='\033[0;31m' 
+7 NC='\033[0m' 
+8 
+ 
+9 #Checking OS 
+10 if [[ $(lsb_release -d) != *16.04* ]]; then 
+11   echo -e ${RED}"The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04."${NC} 
+12   exit 1 
+13 fi 
+14 
+ 
+15 echo -e ${YELLOW}"Welcome to the Northern Automated Install, Durring this Process Please Hit Enter or Input What is Asked."${NC} 
+16 echo 
+17 echo -e ${YELLOW}"You Will See alot of code flashing across your screen, don't be alarmed it's supposed to do that. This process can take up to an hour and may appear to be stuck, but I can promise you it's not."${NC} 
+18 echo 
+19 echo -e ${GREEN}"Are you sure you want to install a Northern Masternode? type y/n followed by [ENTER]:"${NC} 
+20 read AGREE 
+21 
+ 
+22 if [[ $AGREE =~ "y" ]] ; then 
+23 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the first node:"${NC} 
+24 read privkey 
+25 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for second node:"${NC} 
+26 read privkey2 
+27 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the third node:"${NC} 
+28 read privkey3 
+29 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for 4th node:"${NC} 
+30 read privkey4 
 
-#Setup Variables
-GREEN='\033[0;32m'
-YELLOW='\033[0;93m'
-RED='\033[0;31m'
-NC='\033[0m'
-
-#Checking OS
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e ${RED}"The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04."${NC}
-  exit 1
-fi
-
-echo -e ${YELLOW}"Welcome to the Northern Automated Install, Durring this Process Please Hit Enter or Input What is Asked."${NC}
-echo
-echo -e ${YELLOW}"You Will See alot of code flashing across your screen, don't be alarmed it's supposed to do that. This process can take up to an hour and may appear to be stuck, but I can promise you it's not."${NC}
-echo
-echo -e ${GREEN}"Are you sure you want to install a Northern Masternode? type y/n followed by [ENTER]:"${NC}
-read AGREE
-
-if [[ $AGREE =~ "y" ]] ; then
-echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the first node:"${NC}
-read privkey
-echo -e ${GREEN}"Please Enter Your Masternodes Private Key for second node:"${NC}
-read privkey2
-echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the third node:"${NC}
-read privkey3
-echo -e ${GREEN}"Please Enter Your Masternodes Private Key for 4th node:"${NC}
-read privkey4
-echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the 5th node:"${NC}
-read privkey5
+# echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the 5th node:"${NC}
+# read privkey5
 # echo -e ${GREEN}"Please Enter Your Masternodes Private Key for 6th node:"${NC}
 # read privkey6
 # echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the 7th node:"${NC}
